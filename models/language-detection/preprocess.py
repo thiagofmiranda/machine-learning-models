c50193fa459101bfa995b349364a969667d32661
Thiago F. Miranda <33705632+thiagofmiranda@users.noreply.github.com>
2024-11-27 09:27:05 +0000

    fixing model files

--- models/language-detection/preprocess.py
+++ models/language-detection/preprocess.py
@@ -5,7 +5,7 @@ def preprocess_text(text):
     # Remove special characters, punctuation, and numbers
     text = re.sub(r'[!@#$(),\n"%^*?\:;~`0-9]', " ", text)
     # Remove square brackets
-    text = re.sub(r"[[]]", " ", text)
+    text = re.sub(r"[\[\]]", " ", text)
     # Convert text to lowercase
     text = text.lower()
     return text
