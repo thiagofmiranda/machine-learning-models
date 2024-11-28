f8e0896537bb922f9172612f47dd3ae412d4c71f
Thiago F. Miranda <33705632+thiagofmiranda@users.noreply.github.com>
2024-11-28 10:43:02 +0000

    fixing predict function

--- models/language-detection/predict.py
+++ models/language-detection/predict.py
@@ -1,6 +1,6 @@
 import pickle
 from pathlib import Path
-from preprocess import preprocess_text  # Import preprocessing function
+import re
 
 # Define the version and base directory for model loading
 __version__ = "0.1.0"
@@ -9,6 +9,17 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent
 # Load the trained machine learning model
 with open(f"{BASE_DIR}/language-detection-{__version__}.pkl", "rb") as f:
     model = pickle.load(f)
+
+
+# Function to preprocess input text by removing unwanted characters
+def preprocess_text(text: str) -> str:
+    """
+    Function to preprocess input text by removing unwanted characters
+    """
+    text = re.sub(r'[!@#$(),\n"%^*?\:;~`0-9]', " ", text)
+    text = re.sub(r"[\[\]]", " ", text)
+    return text.lower()
+
 
 # Define the list of language classes
 classes = [
@@ -32,9 +43,12 @@ classes = [
 ]
 
 # Function to predict the language of the given text
-def predict_language(text):
+def predict(data):
+    """
+    Function to predict the language of the given text
+    """
     # Preprocess the input text
-    cleaned_text = preprocess_text(text)
+    cleaned_text = preprocess_text(data["text"])
     # Predict the language using the model
     pred = model.predict([cleaned_text])
     # Return the corresponding language class
